1c6d34844d1dd6509bdc4a018532c802a4a8bc73
SilentScream999 <95589247+SilentScream999@users.noreply.github.com>
2026-03-23 23:55:29 -0600

    Update index.ts

--- src/routes/books/index.ts
+++ src/routes/books/index.ts
@@ -2,7 +2,7 @@ import * as Consumet from '@consumet/extensions';
 
 console.log('Consumet exports:', Consumet);
 
-// TEMP: stop using BOOKS so TS compiles
-export default async (req: any, res: any) => {
-  res.send('Check logs for Consumet exports');
-};
+// Minimal valid handler
+export default async function handler(req: any, res: any) {
+  res.send('OK - check logs');
+}
