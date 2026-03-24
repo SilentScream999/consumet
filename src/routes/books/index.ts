b9c77680561c9de7f5b0de7d2c86bb9d7b304eb7
SilentScream999 <95589247+SilentScream999@users.noreply.github.com>
2026-03-23 23:47:35 -0600

    Update index.ts

--- src/routes/books/index.ts
+++ src/routes/books/index.ts
@@ -1,38 +1,8 @@
 import * as Consumet from '@consumet/extensions';
 
 console.log('Consumet exports:', Consumet);
-import { FastifyRequest, FastifyReply, FastifyInstance, RegisterOptions } from 'fastify';
 
-import libgen from './libgen';
-const routes = async (fastify: FastifyInstance, options: RegisterOptions) => {
-  const lbgen = new BOOKS.Libgen();
-
-  fastify.get('/', async (request: any, reply: any) => {
-    reply.status(200).send('Welcome to Consumet Books 📚');
-  });
-
-  fastify.get('/s', async (request: FastifyRequest, reply: FastifyReply) => {
-    const { bookTitle, page } = request.query as {
-      bookTitle: string;
-      page: number;
-    };
-    if (!bookTitle)
-      return reply.status(400).send({
-        message: 'bookTitle query needed',
-        error: 'invalid_input',
-      });
-    try {
-      const data = await lbgen.search(bookTitle, page);
-      return reply.status(200).send(data);
-    } catch (e) {
-      return reply.status(500).send({
-        message: e,
-        error: 'internal_error',
-      });
-    }
-  });
-
-  await fastify.register(libgen, { prefix: '/libgen' });
+// TEMP: stop using BOOKS so TS compiles
+export default async (req: any, res: any) => {
+  res.send('Check logs for Consumet exports');
 };
-
-export default routes;
